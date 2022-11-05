a91675be1ef3e25ece6b760ad2627aaba77f1531
AuthorHashim <hashim.jl@outlook.de>
2022-11-05 19:38:48 +0100

    changes

--- index.js
+++ index.js
@@ -18,37 +18,58 @@ app.get("/", (req,res) => {
     res.json("backend yau")
 })
 
-app.post("/books", (req, res) => {
-    const q = "INSERT INTO books (`title`,`description`,`cover`) VALUES (?)"
-    const values = [
-        req.body.title,
-        req.body.description,
-        req.body.cover
-    ]
-
-    db.query(q, [values], (err,data) => {
-        if (err) return res.json(err)
-        return res.json("Book has ben created succesfully")
-    })
-})
-
-app.get("/books", (req,res) => {
-    const q = "SELECT * FROM books"
+// show all entrys
+app.get("/entry", (req,res) => {
+    const q = "SELECT * FROM entry"
     db.query(q, (err,data) => {
         if (err) return res.json(err)
         return res.json(data)
     })
 })
 
-app.delete("/books/:id", (req,res) => {
+// create a new entry
+app.post("/entry", (req, res) => {
+    const q = "INSERT INTO entry (`username`,`skill`,`category`,`description`,`price`,`rating`) VALUES (?)"
+    const values = [
+        req.body.username,
+        req.body.skill,
+        req.body.category,
+        req.body.description,
+        req.body.price,
+        req.body.rating
+    ]
+
+    db.query(q, [values], (err,data) => {
+        if (err) return res.json(err)
+        return res.json("Entry has ben created succesfully")
+    })
+})
+
+// delete an entry
+app.delete("/entry/:id", (req,res) => {
     const bookId = req.params.id
-    const q = "DELETE FROM books WHERE id = ?"
+    const q = "DELETE FROM entry WHERE id = ?"
 
     db.query(q, [bookId], (err, data) => {
         if (err) return res.json(err)
-        return res.json("Book has been deleted successfully.")
+        return res.json("Entry has been deleted successfully.")
     })
+})
 
+app.put("/entry/:id", (req,res) => {
+    const bookId = req.params.id
+    const q = "UPDATE entry SET `title` = ?, `description` = ?, `cover` = ? WHERE id = ?";
+
+    const values = [
+        req.body.title,
+        req.body.description,
+        req.body.cover
+    ]
+
+    db.query(q, [...values, bookId], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("Book has been updated successfully.")
+    })
 })
 
 app.listen(8800, () => {
